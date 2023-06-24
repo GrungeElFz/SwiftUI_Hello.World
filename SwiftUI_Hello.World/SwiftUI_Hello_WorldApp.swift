479764c01b00eea112dbb17e02bf677e2cd91383
GrungeElFz <elf0114_@hotmail.com>
2023-06-24 11:25:18 -0500

    Initialized:    iOS Template
    Options:    Interface - SwiftUI
                Language - Swift

---
+++ SwiftUI_Hello.World/SwiftUI_Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Hello_WorldApp.swift
+//  SwiftUI_Hello.World
+//
+//  Created by Sahatsawat Kerdsawad on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
